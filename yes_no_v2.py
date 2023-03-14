874840abcec21712c8a0e149f09683f1772d953c
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-15 12:29:14 +1300

    Create yes_no_v2.py

---
+++ yes_no_v2.py
@@ -0,0 +1,19 @@
+# Yes/no checker
+def yes_no(question):
+    while True:
+
+        response = input(question)
+        if response in valid:
+            return
+        else:
+            print("Please enter yes/no")
+
+
+valid = ["yes", "y", "no", "n"]
+show_instructions = yes_no("Have you played this game before? ")
+
+instructions = "Welcome to the math quiz, you can select levels 1 - 3 in which level 1 where boh numbers are under 20" \
+               "addition or subtraction. Level 2 has numbers under 150 and allows addition and subtraction. Level 3 is " \
+               "whole number multiplication/division for numbers below 13"
+if show_instructions == valid[2]:
+    print(instructions)
