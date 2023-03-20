2a7c72def6bf1e0346723711ff1206dfb4609333
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-21 11:39:27 +1300

    Create levels_v2.py

---
+++ levels_v2.py
@@ -0,0 +1,14 @@
+import random
+def choice_checker(question, list, error):
+    while True:
+        response = input(question)
+        if response not in list:
+            print(error)
+        else:
+            print("Program Continues")
+
+valid_level_list = ["1", "2", "3"]
+
+type_of_level = choice_checker("Choose level: ", valid_level_list,
+                               "Please enter a whole integer between 1 and 3")
+
