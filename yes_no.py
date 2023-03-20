f591e7faf3608a6141f4fc6f9d037b673e47940f
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-21 11:39:33 +1300

    Update yes_no.py

--- yes_no.py
+++ yes_no.py
@@ -2,18 +2,26 @@
 def yes_no(question):
     while True:
 
-        response = input(question)
+        response = input(question).lower()
         if response == "yes" or response == "y":
-            return response
+            print("Program Continues")
         elif response == "n" or response == "no":
-            return response
+            print("Shows Instructions")
         else:
             print("Please enter yes/no")
 
+# Asks question to user
 show_instructions = yes_no("Have you played this game before? ")
 
-instructions = "Welcome to the math quiz, you can select levels 1 - 3 in which level 1 where boh numbers are under 20" \
-               "addition or subtraction. Level 2 has numbers under 150 and allows addition and subtraction. Level 3 is " \
-               "whole number multiplication/division for numbers below 13"
-if show_instructions == "no" or show_instructions == "n":
+# Instructions information
+instructions = "Welcome to the math quiz, you can select levels 1 - 3 " \
+               "in which level 1 where boh numbers are under 20"
+instructions_I = "addition or subtraction. Level 2 has numbers under " \
+                 "150 and allows addition and subtraction. Level 3 is "
+instructions_II = "whole number multiplication/division for numbers below 13"
+
+# if user hasnt played game before, show instructions
+if show_instructions == "no":
     print(instructions)
+    print(instructions_I)
+    print(instructions_II)
