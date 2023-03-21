092f445e4891058d506657dba32c726b69766ed6
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-22 11:48:37 +1300

    Create ask_user_questions_v2.py

---
+++ ask_user_questions_v2.py
@@ -0,0 +1,17 @@
+def user_number_questions(question, error, required_numbers=None):
+    while True:
+        try:
+            response = int(input(question))
+            if required_numbers is not None:
+                if response >= required_numbers:
+                    return response
+                else:
+                    print(error)
+                    continue
+            return response
+
+        except ValueError:
+            print(error)
+
+amount_of_questions = user_number_questions("Number of questions: ", "Please enter an integer greater than 0", 1)
+
