343315b7e51726ba9810a602e2a2252d26244d26
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-23 09:08:59 +1300

    Update trials.py

--- trials.py
+++ trials.py
@@ -1,15 +1,6 @@
-def user_number_questions(question, error, required_numbers=None):
-    while True:
-        try:
-            response = int(input(question))
-            if required_numbers is not None:
-                if response >= required_numbers:
-                    return response
-                else:
-                    print(error)
-            return response
+nubers = input("Numbers")
 
-        except ValueError:
-            print(error)
-
-user_number_questions("Guess: ", 'error message')
+if nubers.isnumeric():
+    print("It works")
+else:
+    print("It doesnt work")
