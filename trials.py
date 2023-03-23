eb00cc35a3120f394b93788a47ba02280c7fe3ff
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-24 09:41:51 +1300

    Update trials.py

--- trials.py
+++ trials.py
@@ -1,6 +1,8 @@
-nubers = input("Numbers")
+name = input("Whats your name? ").lower()
 
-if nubers.isnumeric():
-    print("It works")
-else:
-    print("It doesnt work")
+if name == "hamid":
+    question = input("Gday bomber what are your plans for today?(another 9/11) say yes or no: ")
+    if question == "yes":
+        print("BOOM 20k people dead")
+    else:
+        print("Stupid scammer blow yourself up")
