bdcaf3281d0ca5d6e88bd0c3a5ffbcaef10d671b
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-29 12:10:41 +1300

    Update trials.py

--- trials.py
+++ trials.py
@@ -1,8 +1,1 @@
-name = input("Whats your name? ").lower()
-
-if name == "hamid":
-    question = input("Gday bomber what are your plans for today?(another 9/11) say yes or no: ")
-    if question == "yes":
-        print("BOOM 20k people dead")
-    else:
-        print("Stupid scammer blow yourself up")
+print(eval("five plus four is""5 + 4"))
