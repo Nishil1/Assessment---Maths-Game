5cca51ca81930d7c6eec4e77d730979bc3b9e09a
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-03-29 20:52:26 +1300

    Create percentage_generator_V1.py

---
+++ percentage_generator_V1.py
@@ -0,0 +1,12 @@
+
+right = 1
+total_questions = 1
+
+wrong = (total_questions - right)
+
+percentage_right = right / total_questions * 100
+percentage_wrong = wrong / total_questions * 100
+
+
+
+print(f"You got {percentage_right}% right and {percentage_wrong}% wrong.")
